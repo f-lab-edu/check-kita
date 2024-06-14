324cadd682398e44fb06bfeb7d654f051e0ba160
Sumin Park <clappingmin@gmail.com>
2024-06-14 15:56:37 +0900

    feat: 나의 책 인터페이스 정리

---
+++ src/shared/enums/book.enum.ts
@@ -0,0 +1,5 @@
+export enum BookRecordType {
+  already = 'already',
+  ing = 'ing',
+  want = 'want',
+}
--- src/shared/interfaces/book.interface.ts
+++ src/shared/interfaces/book.interface.ts
@@ -1,3 +1,5 @@
+import { BookRecordType } from '../enums/book.enum';
+
 export interface SearchBook {
   title: string;
   link: string;
@@ -8,4 +10,31 @@ export interface SearchBook {
   publisher: string;
   isbn: number;
   pubdate: string;
+}
+
+export interface MyBook {
+  title: string;
+  author: string[];
+  image: string;
+  readingRecord: {
+    recordType: BookRecordType;
+    recordDetail: AlreadyBook | IngBook | WantBook;
+  };
+}
+
+export interface AlreadyBook {
+  startDate: Date;
+  endDate: Date;
+  rating: number;
+}
+
+export interface IngBook {
+  startDate: Date;
+  readingProgressType: 'percentage' | 'pages';
+  readingProgressCount: number;
+}
+
+export interface WantBook {
+  expectationRating: number;
+  expectationMemo: string;
 }
