33ab520916b18b2d5131257f04dbabb4b4fbbd6c
Sumin Park <clappingmin@gmail.com>
2024-06-19 14:31:33 +0900

    style: 브랜드 컬러, 폰트 사이즈 정의

--- src/shared/styles/chakra/index.ts
+++ src/shared/styles/chakra/index.ts
@@ -1,6 +1,10 @@
 import { extendTheme } from '@chakra-ui/react';
 import { modalTheme } from './components/modal';
+import { colors } from './variables/colors';
+import { fontSizes } from './variables/fontSizes';
 
 export const theme = extendTheme({
+  colors: colors,
+  fontSizes: fontSizes,
   components: { Modal: modalTheme },
 });
---
+++ src/shared/styles/chakra/variables/fontSizes.ts
@@ -0,0 +1,7 @@
+export const fontSizes = {
+  xs: '11px',
+  sm: '13px',
+  md: '15px',
+  lg: '18px',
+  xl: '24px',
+};
