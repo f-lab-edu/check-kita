1a384004f07a2661722603ff4b11a6ad7c8605ae
Sumin Park <clappingmin@gmail.com>
2024-07-01 14:04:00 +0900

    fix: vercel build 실패 수정 - 사용하지 않는 코드 제거

--- src/components/search/DateInput.tsx
+++ src/components/search/DateInput.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, useReducer, useState } from 'react';
+import { Dispatch, useReducer } from 'react';
 import styled from 'styled-components';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
